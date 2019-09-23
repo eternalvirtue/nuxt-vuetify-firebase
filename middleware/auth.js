74053accbc2ee4eecf658f27415d297b91dd2457
toku <login@toku.co.uk>
2019-09-24 00:48:50 +0900

    add log methods

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,14 @@
 import firebaseAPI from '~/plugins/firebaseAPI'
+import logUtil from '~/plugins/logUtil'
 
 export default async function ({ store, route, redirect }) {
+  console.log('route.name = ', route.name)
+  if (store.state.Account.user === null) {
+    console.log('store.state.Account.user is null')
+  } else {
+    console.log('store.state.Account.user = ', logUtil.stringify(store.state.Account.user))
+  }
+
   if (route.name === "login" || route.name === "createAccount") {
     if (store.state.Account.user !== null) {
       redirect('/')
@@ -8,18 +16,13 @@ export default async function ({ store, route, redirect }) {
   } else if (route.name === "logout") {
     firebaseAPI.logout().then(store.commit('Account/clearUser')).then(redirect('/login'))
   } else {
-    if (store.state.Account.user === null) {
-      console.log('store2.state.Account.user is null ')
-    } else {
-      console.log('store2.state.Account.user = ' + JSON.stringify(store.state.Account.user))
-    }
     if (store.state.Account.user === null ) {
       await firebaseAPI.auth().then(user => {
-        console.log('state2 auth user ' + JSON.stringify(user))
+        console.log('state auth user ', logUtil.stringify(user))
         if (!user) {
           redirect('/login');
         } else {
-          store.commit('Account/setUser', {
+          store.dispatch('Account/setUser', {
             uid: user.uid,
             email: user.email,
             username: user.displayName,
