c9189641f7e3ef99edae217e1cbc5089d6a08de6
toku <login@toku.co.uk>
2019-09-24 00:47:03 +0900

    add methods

--- plugins/firebaseAPI.js
+++ plugins/firebaseAPI.js
@@ -1,26 +1,49 @@
 import firebase from '@/plugins/firebase'
 import 'firebase/auth'
+import 'firebase/firestore'
+
+const firestore = firebase.firestore()
+const auth = firebase.auth()
 
 const firebaseAPI = {
   login(email, password) {
     return new Promise((resolve, reject) => {
-      firebase.auth().signInWithEmailAndPassword(email, password)
+      auth.signInWithEmailAndPassword(email, password)
         .then(user => resolve(user))
         .catch(err => reject(err))
     })
   },
   logout() {
     return new Promise((resolve, reject) => {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => resolve())
-        .catch(err => reject(err))
+      auth.signOut()
+      .then(() => resolve())
+      .catch(err => reject(err))
     })
   },
   auth() {
     return new Promise((resolve, reject) => {
-      firebase.auth().onAuthStateChanged(user => resolve(user))
+      auth.onAuthStateChanged(user => resolve(user))
+    })
+  },
+  fetchNotes(userid) {
+    return new Promise((resolve, reject) => {
+      firestore.collection("notes").doc(userid).collection('entry').get()
+      .then(querySnapshot => {
+        resolve(querySnapshot)
+      }).catch(err => {
+          reject(err)
+      })
+    })
+  },
+  addNote(note, userid) {
+    return new Promise((resolve, reject) => {
+      note.timestamp = firebase.firestore.FieldValue.serverTimestamp()
+      firestore.collection("notes").doc(userid).collection('entry').add(note)
+      .then(newData => {
+        resolve(newData)
+      }).catch(err => {
+        reject(err)
+      })
     })
   }
 }
