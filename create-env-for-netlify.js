b2f2c66e27c832ac07426aeed9a643f93aa84080
toku <login@toku.co.uk>
2019-09-30 23:38:53 +0900

    add script for netlify

---
+++ create-env-for-netlify.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+fs.writeFileSync('./.env44', `LOGMODE=DEV\n
+API_KEY=${process.env.API_KEY}\n
+AUTH_DOMAIN=${process.env.AUTH_DOMAIN}\n
+DATABASE_URL=${process.env.DATABASE_URL}\n
+PROJECT_ID=${process.env.PROJECT_ID}\n
+STORAGE_BUCKET=${process.env.STORAGE_BUCKET}\n
+MESSAGING_SENDER_ID=${process.env.MESSAGING_SENDER_ID}\n`)
+
